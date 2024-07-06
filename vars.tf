821e25bcff613e3abf8d5f70bdcf467f943fcd53
pavan <balubadi.pavankumar@gmail.com>
2024-07-06 15:46:28 +0530

    expense-terraform1

--- vars.tf
+++ vars.tf
@@ -1,12 +1,13 @@
+
 variable "vpc_cidr_block" {}
+variable "env" {}
+variable "tags" {}
 variable "public_subnets" {}
 variable "web_subnets" {}
 variable "app_subnets" {}
 variable "db_subnets" {}
 variable "azs" {}
-variable "tags" {}
-variable "env" {}
-variable "default_vpc_cidr" {}
 variable "account_id" {}
 variable "default_vpc_id" {}
 variable "default_route_table_id" {}
+variable "default_vpc_cidr" {}
